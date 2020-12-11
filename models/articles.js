c2267b05293866f4d5c23e09667ec1410899ac1e
Jasmine Snow <jasminesimmone@gmail.com>
2020-12-10 16:14:26 -0800

    post errors

--- models/articles.js
+++ models/articles.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const articlesSchema = mongoose.Schema({
-  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  // userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
   title: {type: String, required: false},
   img: {type: String, required: false},
   description: {type: String, required: false},
